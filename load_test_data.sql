c9d748f988711c8315a2337233e9051101093600
Connie00-hello <itsconnie.ckn@gmail.com>
2024-09-20 16:01:44 +0800

    Add files via upload

---
+++ load_test_data.sql
@@ -0,0 +1,19 @@
+copy PREDICTIVEMAINT.readings (
+    id filler int,
+        asset_id,
+        unixtimestamp,
+        asset_status ,
+        temperature_sensor,
+        temperature ,
+        powerfactor_sensor,
+        powerfactor,
+        airflow_sensor,
+        airflow,
+        pressure_sensor,
+        pressure,
+        vibration_sensor,
+        vibration,
+        maintenance,
+        batch_id
+)
+from  '/home/dbadmin/TestDrive/PredictiveMaint/Test_Data.csv'  delimiter ',' null ''  enclosed by '"';
